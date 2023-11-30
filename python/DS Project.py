d8a6564b5ca873bf729c740c1877a9dc481ac617
tanyuzhou <tyz@gztyz.com>
2023-11-30 00:42:50 +0000

    Update DS Project.py

--- python/DS Project.py
+++ python/DS Project.py
@@ -1,76 +1,63 @@
 """
-Given a connected weighted graph (in weighted neighboring matrix), return the shortest path
+Given a connected indirected graph in weighted Adjacency matrix, source node, and terminal node
+will return the shortest path and the distance
 """
 
 
-def dijkstra(weighted_matrix, start, end):
-
-    def updateU(vertex, U):
-        visited[vertex] = True
-        for i in range(len(weighted_matrix[vertex])):
-            if weighted_matrix[vertex][i] != _:
-                if U[i] == _ or weighted_matrix[vertex][i] + U[vertex] < U[i]:
-                    U[i] = weighted_matrix[vertex][i] + U[vertex]
-                    path[i] = vertex
-        return U
-
-    num_vertices = len(weighted_matrix)
-
-    # record the previous node in path
-    path = [_] * num_vertices
-
-    # record choosing all vertices
+def dijkstra(adj_matrix, start, end):
+    num_vertices = len(adj_matrix)
+    distances = [float('inf')] * num_vertices
+    distances[start] = 0
+    path = [-1] * num_vertices
     visited = [False] * num_vertices
 
-    # initialization of U
-    U = [_] * num_vertices
-    U[start] = 0
+    def updateU():
+        # Update distances of adjacent vertices
+        for neighbor, weight in enumerate(adj_matrix[current_vertex]):
+            if weight != _ and not visited[neighbor]:
+                distance = min_distance + weight
+                if distance < distances[neighbor]:
+                    distances[neighbor] = distance
+                    path[neighbor] = current_vertex
+        print("distances:", distances)
 
-    current = start
+    for i in range(num_vertices):
+        print('-'*100)
+        # Find the vertex with the minimum distance to the source point
+        min_distance = float('inf')
+        for v in range(num_vertices):
+            if not visited[v] and distances[v] < min_distance:
+                min_distance = distances[v]
+                current_vertex = v
+        print("current vertex:", current_vertex)
+        visited[current_vertex] = True
+        updateU()
 
-    # Getting U
-    while False in visited:
-        updateU(current, U)
-        print("-"*100)
-        print("visited:", visited)
-        print("path:", path)
-        w_min = _
-        v_min = None
-        print("distance:", U)
-        for i in range(len(weighted_matrix[current])):
-            if weighted_matrix[current][i] < w_min and not visited[i]:
-                w_min = weighted_matrix[current][i]
-                v_min = i
-        current = v_min
-        print("chosen:", current)
-        if not current:
-            break
-
-    # Reconstructing path using recursion
+    # Reconstruct the path and calculate total distance
+    total_distance = distances[end]
     shortest_path = []
-
-    def rec_reconstruction(ind):
-        shortest_path.append(path[ind])
-        if ind == start:
-            return None
-        rec_reconstruction(path[ind])
-
-    rec_reconstruction(end)
+    while end != -1:
+        shortest_path.append(end)
+        end = path[end]
     shortest_path.reverse()
-    shortest_path = shortest_path[1:] + [end]
     print("shortest path:", shortest_path)
-    return U, shortest_path
+    print("shortest distance:", total_distance)
+
+    return shortest_path, total_distance
 
 
 _ = float('inf')
-weighted_matrix = [
-    # [_, 12, _, _, _, 16, 14],
-    # [12, _, 10, _, _, 7, _],
-    # [_, 10, _, 3, 5, 6, _],
-    # [_, _, 3, _, 4, _, _],
-    # [_, _, 5, 4, _, 2, 8],
-    # [16, 7, 6, _, 2, _, 9],
-    # [14, _, _, _, 8, 9, _]
+weighted_matrix1 = [
+    [_, 12, _, _, _, 16, 14],
+    [12, _, 10, _, _, 7, _],
+    [_, 10, _, 3, 5, 6, _],
+    [_, _, 3, _, 4, _, _],
+    [_, _, 5, 4, _, 2, 8],
+    [16, 7, 6, _, 2, _, 9],
+    [14, _, _, _, 8, 9, _]
+]
+
+weighted_matrix2 = [
     [_, 3, 7, 4, _, _, _],
     [3, _, 2, _, _, 7, _],
     [7, 2, _, 8, 1, _, _],
@@ -80,6 +67,16 @@ weighted_matrix = [
     [_, _, _, 2, _, 1, _]
 ]
 
-start = 6
-end = 0
-dijkstra(weighted_matrix, start, end)
+weighted_matrix3 = [
+    [_, 4, 2, 4, _, _, _],
+    [4, _, 2, _, _, 1, _],
+    [2, 2, _, 5, 1, _, _],
+    [4, _, 5, _, _, _, 2],
+    [_, _, 5, _, _, _, _],
+    [_, 1, _, _, _, _, 2],
+    [_, _, _, 2, _, 2, _]
+]
+
+start = 0
+end = 6
+dijkstra(weighted_matrix1, start, end)
